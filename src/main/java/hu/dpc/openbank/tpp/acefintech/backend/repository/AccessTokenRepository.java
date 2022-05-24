a80e5f2db65652f5d3e99db013339e56af6c8bac
Vic Romero <victor.romero@fintecheando.mx>
2022-05-23 19:21:17 -0500

    Debug

--- src/main/java/hu/dpc/openbank/tpp/acefintech/backend/repository/AccessTokenRepository.java
+++ src/main/java/hu/dpc/openbank/tpp/acefintech/backend/repository/AccessTokenRepository.java
@@ -41,7 +41,7 @@ public interface AccessTokenRepository extends JpaRepository<AccessToken, String
      */
     @Transactional
     @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query(value = "delete from access_token a\n" //
+    @Query(value = "delete from access_token \n" //
             + "      where USERNAME = :username\n" //
             + "        and BANK_ID = :bankid\n"  //
             + "        and ACCESS_TOKEN_TYPE = 'user'", nativeQuery = true)
